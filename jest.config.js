3544ea9164fdd403a948da4dff7225b7e754351a
constfold <65550294+constfold@users.noreply.github.com>
2021-06-27 14:05:08 +0800

    Fix format

--- jest.config.js
+++ jest.config.js
@@ -149,10 +149,7 @@ module.exports = {
     testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    testPathIgnorePatterns: [
-        "/node_modules/",
-        "utils.ts"
-    ],
+    testPathIgnorePatterns: ["/node_modules/", "utils.ts"],
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
     // testRegex: [],
